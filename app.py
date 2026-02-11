27f4f71f2ffd74e0f9cf5c97432c61a11e06d5db
jackterhar <jack.terharoutunian@gmail.com>
2026-02-11 14:52:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import yfinance as yf
+import matplotlib.pyplot as plt
+
 # =========================================================
 # LOAD (SPY-ANCHORED)
 # =========================================================
